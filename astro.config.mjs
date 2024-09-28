766c2a0790bd9b507d0888872824be0aa8863221
Vincent Udén <vincent.uden@colmeo.se>
2024-09-28 18:42:06 +0200

    Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import vercelStatic from "@astrojs/vercel/static";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  output: "static",
+  adapter: vercelStatic(),
+  integrations: [tailwind()],
 });
