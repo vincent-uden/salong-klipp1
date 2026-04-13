18d8eb03885451cf41b267ee9b5a32f3d94606ac
Vincent Udén <vincentuden@gmail.com>
2026-04-13 13:24:50 +0200

    Enable uden analytics and switch to serverless adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,20 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import vercelStatic from "@astrojs/vercel/static";
 import tailwind from "@astrojs/tailwind";
+import vercelServerless from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
-  adapter: vercelStatic({
+  output: "server",
+  adapter: vercelServerless({
     webAnalytics: {
       enabled: true,
-    }
+    },
   }),
   integrations: [tailwind()],
   redirects: {
     "/behandlingar": "/",
     "/behandlingar.html": "/",
-  }
+  },
 });
